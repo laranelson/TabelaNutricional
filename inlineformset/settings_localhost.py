112ca5388e6e4ce53ad08be32ea2ef1939c1c6c0
Nelson Lara <nelsonlarajr@gmail.com>
2023-11-26 16:57:39 -0300

    atualização

--- inlineformset/settings_localhost.py
+++ inlineformset/settings_localhost.py
@@ -89,15 +89,15 @@ WSGI_APPLICATION = 'inlineformset.wsgi.application'
 #}
 
 #DATABASE_HEROKU
-DATABASES= {
-    'default': dj_database_url.config()
-}
+#DATABASES= {
+#    'default': dj_database_url.config()
+#}
 
 #prod_db  =  dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(prod_db)
 
 #BASE DE DADOS LOCAL
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db-nutritiontable',
@@ -106,7 +106,7 @@ DATABASES= {
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}"""
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -129,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -198,3 +198,5 @@ EMAIL_HOST_USER = "nelsonlarajr@gmail.com"  # Substitua com seu e-mail completo
 
 # Insira a senha específica gerada pelo Google para aplicativos de terceiros
 EMAIL_HOST_PASSWORD = "wlbgabslyyfckakn"  # Substitua com a senha gerada pelo Google
+
+
